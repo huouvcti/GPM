595e5ff3efb39880d6b295f2298f3b328e98c105
huouvcti <huouvcti@gmail.com>
2022-08-08 13:59:16 +0900

    기능 구현

---
+++ controller/mainCtrl.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const main = async (req, res) => {
+
+    res.render('../views/main.ejs');
+}
+
+
+
+
+module.exports = {
+    main
+}
---
+++ routes/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+
+const mainCtrl = require("../controller/mainCtrl");
+
+
+router.get("/", mainCtrl.main);
+
+
+module.exports = router;
